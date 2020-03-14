9939c6f9194b027f15301cf537586ca06f47d97d
Hunter Digi Ace <hunter-digi-ace@users.noreply.github.com>
2020-03-14 15:51:31 +0100

    3.1 echo

--- lab3/src/main/java/upf/edu/util/LanguageMapUtils.java
+++ lab3/src/main/java/upf/edu/util/LanguageMapUtils.java
@@ -2,10 +2,19 @@ package upf.edu.util;
 
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
+import scala.Tuple2;
+
+import java.util.Arrays;
 
 public class LanguageMapUtils {
 
     public static JavaPairRDD<String, String> buildLanguageMap(JavaRDD<String> lines) {
-        return null;// IMPLEMENT ME
+        // IMPLEMENT ME
+        JavaPairRDD<String, String> languajes = lines
+                .map(s -> Arrays.asList(s.split("\\t")))
+                .filter(s -> s.get(1).length() == 2)
+                .mapToPair(s -> new Tuple2<String, String>(s.get(1),s.get(2)))
+                .coalesce(1);
+        return languajes;
     }
 }
