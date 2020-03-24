f753ea86b296579dc8b0320954f4417a7038e1b3
Daniel González <daniel.gonzalez14@estudiant.upf.edu>
2020-03-24 02:28:25 +0100

    6.1 arreglado pequeño fallo

--- lab3/src/main/java/upf/edu/storage/DynamoHashTagRepository.java
+++ lab3/src/main/java/upf/edu/storage/DynamoHashTagRepository.java
@@ -25,9 +25,6 @@ import java.util.regex.Pattern;
 
 public class DynamoHashTagRepository implements IHashtagRepository, Serializable {
 
-
-
-
   @Override
   public void write(Status h) { // IMPLEMENT ME
 
@@ -42,8 +39,6 @@ public class DynamoHashTagRepository implements IHashtagRepository, Serializable
     final DynamoDB dynamoDB = new DynamoDB(client);
     final Table dynamoDBTable = dynamoDB.getTable("LSDS2020-TwitterHashtags");
 
-    ///////////////////////////////ejemplo de prueba, esto deberia crear algo en la base de datos
-
     List<String> hashtag =  getHashtags(h.getText());//hacer un for por palabra con hastag
     String lang = h.getLang();
     Long id = h.getId();
@@ -52,8 +47,6 @@ public class DynamoHashTagRepository implements IHashtagRepository, Serializable
 
       UpdateItemSpec updateItemSpec = new UpdateItemSpec().withPrimaryKey("hashtag", hashtag.get(i), "lang", lang)
               .withUpdateExpression("set contador = contador + :r,  ids = list_append(ids, :i)")
-              //set info.rating = info.rating + :val
-              //ADD past_visits :inc, past_chats :inc  SET reset_time = :value
               .withValueMap(new ValueMap().withNumber(":r", 1).withList(":i", Arrays.asList(id)))
               .withReturnValues(ReturnValue.UPDATED_NEW);
 
